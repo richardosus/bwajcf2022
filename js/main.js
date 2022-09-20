a9ca061a26bc12d9e8a2d21aecb60a828d7ce2f9
richardosus <richardosusilo01@gmail.com>
2022-09-20 16:17:16 +0700

    Belajar Array JavaScript

--- js/main.js
+++ js/main.js
@@ -6,7 +6,14 @@ let jumlahBox = 5;
 let wallet = 10000;
 let hutang = 9000;
 
+// tipe data array
+// jumlah anak di array dihitung dari 0 bukan 1
+let telurFoods = ['Gado-gado', 'Ketoprak', 'BMW', 'Omelette', 'Jengkol', 'Bubur Ayam'];
+// mendeklarasikan data yang tidak sesuai array dengan metode replace
+telurFoods[2] = "Pancake";
+// .push untuk menambahkan array di akhir
+telurFoods.push("Sate Telur");
+
 // Menampilkan data
-console.log(boxSaya);
-console.log(jumlahBox);
-console.log(wallet>=hutang);
+console.log(telurFoods[2]);
+console.log(telurFoods);
