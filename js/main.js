74397ee0ddb464dfcc89ced935d96e4d45df141d
richardosus <richardosusilo01@gmail.com>
2022-09-20 16:05:20 +0700

    Belajar Tipe Data String, Number dan Boolean

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-// Saya sedang belajar JavaScript terbaru
-// Mengenal console log di Browser
-console.log("Belajar JavaScript di BWA");
+// Tipe data String
+let boxSaya = 'Sepatu';
+// Tipe data Number
+let jumlahBox = 5;
+// Tipe data Boolean
+let wallet = 10000;
+let hutang = 9000;
+
+// Menampilkan data
+console.log(boxSaya);
+console.log(jumlahBox);
+console.log(wallet>=hutang);
