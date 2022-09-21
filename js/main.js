68de00ede8eea84ef130d4a9fce45e18ee1239ed
richardosus <richardosusilo01@gmail.com>
2022-09-21 15:17:39 +0700

    Belajar Functions JavaScript

--- js/main.js
+++ js/main.js
@@ -1,17 +1,14 @@
-// Belajar Logical Operator
-let usernameDatabase = "hujan";
-let passwordDatabase = "123aaa";
+// Belajar Function
+// function adalah sebuah container yang didesain untuk menjalankan berbagai aksi
+function menyimpanProgresBelajar() {
+    // mengambil data progres saat ini
+    let progresBelajar = 55;
+    // mengambil data progres tambahan hari ini
+    let progresTambahan = 10;
+    // mengkalkulasi ulang data progres
+    progresBelajar = progresBelajar + progresTambahan;
+    // Menampilkan data
+    console.log(progresBelajar);
+}
 
-let usernameDariInputUser = "hujan";
-let passwordDariInputUser = "111aaa";
-
-let validasiDataUser = usernameDariInputUser == usernameDatabase && passwordDariInputUser == passwordDatabase;
-
-let cuaca1 = "hujan"
-let cuaca2 = "tidak bersalju"
-
-let pakeJaket = cuaca1 == "hujan" || cuaca1 == "tidak bersalju";
-
-// Menampilkan data
-console.log(validasiDataUser);
-console.log(pakeJaket)
+menyimpanProgresBelajar();
