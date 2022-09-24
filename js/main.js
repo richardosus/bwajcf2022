c57bcbbd7ce8c95e47819a4b94cf4738ae342b9e
richardosus <richardosusilo01@gmail.com>
2022-09-24 21:34:03 +0700

    Mempelajari If Else Conditional Statements

--- js/main.js
+++ js/main.js
@@ -2,8 +2,23 @@
 // function adalah sebuah container yang didesain untuk menjalankan berbagai aksi
 function checkUserData(username, password) {
     let usernameDb = 'richardo';
+    let usernameDb2 = 'richardo2';
     let passwordDb = '123aaa';
+    let passwordDb2 = '123';
 
-    let validasi = username == usernameDb && password == passwordDb;
-    alert(validasi);
+    if (username == usernameDb) {
+        if (password == passwordDb) {
+            alert('Berhasil Login')
+        } else{
+            alert('Password salah')
+        }
+    } else if(username == usernameDb2){
+        if (password == passwordDb2) {
+            alert('Berhasil Login Username2')
+        } else {
+            alert('Password Username2 Salah')
+        }
+    } else{
+        alert('Username salah')
+    }
 }
