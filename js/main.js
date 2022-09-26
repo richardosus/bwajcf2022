6ab87ba6a43800ad829208fdad9fe86bcd9ef071
richardosus <richardosusilo01@gmail.com>
2022-09-26 12:36:14 +0700

    Belajar Typeof JavaScript

--- js/main.js
+++ js/main.js
@@ -1,17 +1,11 @@
-// Belajar While
+// typeof
 
-let kelasPremium = ['Blender', 'Laravel', 'React JS', 'Flutter', 'Swift UI']
+let namaUser = 'Richardo';
+let mobil = ['bmw', 'audi', 'jeep']
+let isPremium = true;
 
-// Menampilkan hasil
+console.log(typeof(mobil));
 
-// for (let i = 2; i < kelasPremium.length; i++) {
-//     const element = kelasPremium[i];
-//     console.log(element);
-// }
-
-let i = 0;
-
-while (i<kelasPremium.length) {
-    console.log(kelasPremium[i]);
-    i++;
+if (isPremium == true) {
+    alert('Wecome to member')
 }
